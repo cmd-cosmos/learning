4e368045cf394a63780c64bb849c64e2ae4fb7f6
Premanshu Bhagat <premtsapj@gmail.com>
2025-08-11 23:58:19 -0400

    started constructor member init lists for member function initialization via class constructor.

---
+++ tutorials/cpp/cherno/member-initalizer-list.cpp
@@ -0,0 +1,16 @@
+// constructor member initializer lists for class constructors
+// it is a way to initialize the class member functions inside the constructor.
+
+#include <iostream>
+#include <string>
+
+class Entity
+{
+
+};
+
+int main()
+{
+    
+    return 0;
+}
