2e5eb281386ba3019eba7fcd5e54a532026d234d
Premanshu Bhagat <premtsapj@gmail.com>
2025-08-13 14:23:06 -0400

    started with object initialization, stack vs heap instantiation.

---
+++ tutorials/cpp/cherno/objects.cpp
@@ -0,0 +1,14 @@
+// objects creation methods: the choices depend on the source of memory.
+// which memory are the objects instantiated in ---> stack or heap
+/*
+stack objects:
+lifespan ---> fixed and dependent on the scope
+once out of the scope ---> stack objects are destroyed.
+*/ 
+#include <iostream>
+#include <string>
+
+int main()
+{
+    return 0;
+}
