790512c86026665a25d77ffed03b85cdf7f8fec7
Premanshu Bhagat <premtsapj@gmail.com>
2025-08-14 19:44:11 -0400

    stack allocation of objects.

--- tutorials/cpp/cherno/objects.cpp
+++ tutorials/cpp/cherno/objects.cpp
@@ -8,7 +8,28 @@ once out of the scope ---> stack objects are destroyed.
 #include <iostream>
 #include <string>
 
-int main()
+using String = std::string;
+
+class Entity
 {
+private:
+    String m_Name;
+public:
+    Entity() : m_Name("Unknown") {} // MIL assigns unknown for default constructor 
+    Entity(const String& name) : m_Name(name) {} // MIL to assign name to m_Name if passed to the constructor
+    const String& GetName() const {return m_Name;}
+};
+
+int main()
+{ 
+    //Stack object creation ---> used most often unless obj req for a scope agnostic lifespan
+    // stack also has very limited memory --> 1 MB or 2 MB generally
+    Entity stk_entity; // this calls the default constructor
+    std::cout << stk_entity.GetName() << '\n';
+    
+    Entity stk_e("Batman"); // this is the parametrized constructor
+    std::cout << stk_e.GetName() << '\n';
+
+    
     return 0;
 }
