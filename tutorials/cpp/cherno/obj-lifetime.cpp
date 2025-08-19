c7aceba98ac6fc0788207c449da0caa642da7460
Premanshu Bhagat <premtsapj@gmail.com>
2025-08-19 16:09:31 -0400

    added scope bounded heap allocation using scoped pointers.

--- tutorials/cpp/cherno/obj-lifetime.cpp
+++ tutorials/cpp/cherno/obj-lifetime.cpp
@@ -17,6 +17,31 @@ class Entity
         }
 };
 
+int* CreateArray()
+{
+    // wrong way to create array for external use
+    // the array mem is cleaned as soon as function returns
+
+    int array[50];
+    return array;
+}
+
+class ScopedPtr
+// essentially an implementation of the unique ptr.
+{
+    private:
+        Entity*  m_Ptr;
+    public:
+        ScopedPtr(Entity* ptr) : m_Ptr(ptr)
+        {
+
+        }
+        ~ScopedPtr()
+        {
+            delete m_Ptr;
+        }
+};
+
 int main()
 {
     {
@@ -24,7 +49,19 @@ int main()
         Entity e;   // constructor called
     }               // destructor called here
     // stack entity obj destroyed
-    
+     
+    // heap allocation ---> Entity creation is scope independent
+    // obj not deleted as not explicitly mentioned.
+    // OS responsible for mem cleanup after program termination.
+    {
+        Entity* e = new Entity;
+    }
+
+    {
+        // this will essentially heap alloc the obj but will make it scope bound as the ptr is a unique or scoped ptr that is not preserved out of scope.
+        ScopedPtr e = new Entity();
+    }
+
     return 0;
 }
 
