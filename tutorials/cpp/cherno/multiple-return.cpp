a62d7cee0aa352479b8fb322c23709643c122356
Premanshu Bhagat <premtsapj@gmail.com>
2025-08-24 12:03:40 -0400

    added tuple examples.

--- tutorials/cpp/cherno/multiple-return.cpp
+++ tutorials/cpp/cherno/multiple-return.cpp
@@ -8,19 +8,29 @@
 // tuples ---> fixed number of values, different data types allowed
 
 std::tuple<int, std::string> Get(int x)
+// function that returns a tuple obj. essentailly more than 1 return from a function
 {
     std::tuple<int, std::string> tup = {x,"Batman"};
     return tup;
 }
 
-std::ostream& operator<<(std::ostream& os, std::tuple<int, std::string>& tup)
+std::tuple<int, std::string> TimeToLaunch(int a)
 {
-    os << std::get<0>(tup) << ',' << std::get<1>(tup);
+    std::tuple<int, std::string> tup = std::make_tuple(a, "seconds to launch"); 
+    return tup;
+
+}
+
+std::ostream& operator<<(std::ostream& os, const std::tuple<int, std::string>& tup)
+{
+    os << std::get<0>(tup) << ' ' << std::get<1>(tup);
     return os;
-};
+}
 
 int main()
 {
-    std::cout << Get(5);
+    std::cout << Get(5) << std::endl;
+    std::cout << TimeToLaunch(1500) << std::endl;
+
     return 0;
 }
