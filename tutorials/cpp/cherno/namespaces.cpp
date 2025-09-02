82ea6ebdaa720e3766ee82385b68acdf12ab7278
Premanshu Bhagat <premtsapj@gmail.com>
2025-09-01 22:51:51 -0400

    no major upgrades

--- tutorials/cpp/cherno/namespaces.cpp
+++ tutorials/cpp/cherno/namespaces.cpp
@@ -19,10 +19,20 @@ namespace falcon
     }
 }
 
+namespace batman
+{
+    // same function name as provided by the standard library
+    void cout()
+    {
+        std::cout << "I am Batman" << std::endl;
+    }
+}
+
 int main()
 {
     using namespace falcon;
     using namespace starship;
+    using namespace batman;
 
     // 2 functions with the same name ---> but different functionalities
     // namespaces defines the functionality of the member associated with the namespace.
@@ -30,6 +40,8 @@ int main()
     falcon::launch();
     starship::launch();
     // launch(); // leads to error -->more than one instance of overloaded function "launch" matches the argument list:C/C++(308)
+
+    cout(); // since I have not used std namesapce I can overload the name and also call the cout function in the batman namespace without using the scope resolution operator
 
     return 0;
 }
