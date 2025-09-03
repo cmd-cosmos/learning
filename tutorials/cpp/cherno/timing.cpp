bea7722704996cc9c921f6b92f5ca71d255ef7ac
Premanshu Bhagat <premtsapj@gmail.com>
2025-09-03 16:30:03 -0400

    basic timer and using object lifetime to time function execution time using a Timer Struct.

---
+++ tutorials/cpp/cherno/timing.cpp
@@ -0,0 +1,59 @@
+// timing for benchmarking or performance evaluation or defining action timestamp.
+// use chrono ---> std lib feature
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+struct Timer
+{
+    // utilize object lifetime to create and destroy timer object for individual scopes where the timer object is instantiated
+    // std::chrono::time_point<std::chrono::steady_clock> start, end;
+    std::chrono::duration<float> duration;
+    std::chrono::time_point<std::chrono::_V2::system_clock> start, end;
+
+    Timer()
+    {
+        start = std::chrono::high_resolution_clock::now();
+    }
+    ~Timer()
+    {
+        end = std::chrono::high_resolution_clock::now();
+        duration = end - start;
+        float ms = duration.count() * 1000.0f;
+        std::cout << "Timer took: " << ms << "ms" << std::endl; 
+    }
+};
+
+
+// how long does cout take to print the statements to console in the foo function.
+void foo()
+{
+    Timer timer;
+    for (int i; i < 100; i++)
+    {
+        std::cout << "I am Batman" << std::endl;
+    }
+}
+
+int main()
+{
+    #if 0
+    {
+        using namespace std::literals::chrono_literals;
+        // current time stamp
+        auto start = std::chrono::high_resolution_clock::now();
+        // making the main thread sleep for a second
+        std::this_thread::sleep_for(1s);
+        // ending time stamp
+        auto end = std::chrono::high_resolution_clock::now();
+        // calculating duration with the correct type
+        std::chrono::duration<float> duration = end - start;
+        // duration in seconds
+        std::cout << duration.count() << 's' << std::endl;  
+    }
+    #endif
+
+    foo();
+    
+    return 0;
+}
