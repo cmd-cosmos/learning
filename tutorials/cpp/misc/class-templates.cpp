ac26bfd5fab0fad4946a4af6a64f5f94ce5ad6e9
Premanshu Bhagat <premtsapj@gmail.com>
2025-09-13 13:19:33 -0400

    added class template to create different types of array from single class declaration

--- tutorials/cpp/misc/class-templates.cpp
+++ tutorials/cpp/misc/class-templates.cpp
@@ -26,9 +26,40 @@ class intArray
         }
 };
 
+template<typename T>
+class Array
+{
+    public:
+        T array[10];
 
+        void fill(T val)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = val;
+            }
+        }
+
+        T& at(int idx)
+        {
+            return array[idx];
+        }
+
+};
 
 int main()
 {
+    // the Array class produces homogenous and consistent arrays
+    // only 1 value populated throughout the array
+    Array<std::string> strArray;
+    strArray.fill("Batman");
+    Array<int> intArray;
+    intArray.fill(10);
+    std::cout << strArray.at(3) << std::endl;
+    std::cout << strArray.at(4) << std::endl;
+    std::cout << strArray.at(3) << std::endl;
+    std::cout << intArray.at(1) << std::endl;
+    std::cout << intArray.at(9) << std::endl;
+    std::cout << intArray.at(0) << std::endl;
     return 0;
 }
