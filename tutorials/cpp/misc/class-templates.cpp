8d540a5b71d6fb1256c16b93b0550480a02ff1d7
Premanshu Bhagat <premtsapj@gmail.com>
2025-09-13 13:31:09 -0400

    added function that allows assignment of different values at different indices of the array

--- tutorials/cpp/misc/class-templates.cpp
+++ tutorials/cpp/misc/class-templates.cpp
@@ -45,6 +45,10 @@ class Array
             return array[idx];
         }
 
+        void fillUnique(int idx, T val)
+        {
+            array[idx] = val;
+        }
 };
 
 int main()
@@ -55,11 +59,38 @@ int main()
     strArray.fill("Batman");
     Array<int> intArray;
     intArray.fill(10);
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << intArray.at(i) << std::endl;
+    }
     std::cout << strArray.at(3) << std::endl;
     std::cout << strArray.at(4) << std::endl;
     std::cout << strArray.at(3) << std::endl;
-    std::cout << intArray.at(1) << std::endl;
-    std::cout << intArray.at(9) << std::endl;
-    std::cout << intArray.at(0) << std::endl;
+
+    Array<std::string> characters;
+    characters.fillUnique(0,"Batman");
+    characters.fillUnique(1,"Joker");
+    characters.fillUnique(2,"Alfred");
+    characters.fillUnique(3,"Nighwing");
+    characters.fillUnique(4,"Robin");
+    characters.fillUnique(5,"Bruce");
+    characters.fillUnique(6,"2-Face");
+    characters.fillUnique(7,"Riddler");
+    characters.fillUnique(8,"Cobblepot");
+    characters.fillUnique(9,"Scarecrow");
+    for(int i = 0; i < 10; i++)
+    {
+        std::cout << characters.at(i) << std::endl;
+    }
+    
+    Array<int> nums;
+    for (int i = 0; i < 10; i++)
+    {
+        nums.fillUnique(i,i);
+    } 
+    for (int j = 0; j < 10; j++)
+    {
+        std::cout << nums.at(j) << std::endl;
+    }
     return 0;
 }
