4cadebeaf19faeba0f624d7bcadfd35436e7da35
Premanshu Bhagat <premtsapj@gmail.com>
2025-09-15 19:31:59 -0400

    basic structs and aliasing techniques

--- tutorials/c/misc/structs/structs-detail.c
+++ tutorials/c/misc/structs/structs-detail.c
@@ -1,21 +1,45 @@
-// structs in depth ---> with detailed theory.
+// structs in depth ---> theory in the notes.txt file.
 // knowledge source: https://www.youtube.com/watch?v=qqtmtuedaBM
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-struct Coordinate
+// another method to alias struct type alongside the struct declaration and definition
+typedef struct Coordinate
 {
     // 2D point representation struct
-    float x,y;
-};
+    // 16 bytes reserved
+    double x,y;
+} coordinates;
 
-typedef struct Coordinate coordinates;
+// typedef struct Coordinate coordinates;
+
+void Print_Coords(const coordinates* self);
 
 int main(int argc, char* argv[])
 {
-    coordinates point0 = {0.0f, 0.0f};
-    printf("(%f,%f)\n", (point0.x,point0.y));
+
+    // #if 0
+    coordinates* point0 = malloc(sizeof(coordinates));
+    point0->x = 1.0f;
+    point0->y = 2.0f;
+    Print_Coords(point0);
+    // #endif
+
+    // other method to create a struct type member and pass by ref to the print function
+    coordinates point_A;
+    point_A.x = 100.0f;
+    point_A.y = 200.0f;
+    Print_Coords(&point_A);
+
+    coordinates point_origin = { 0 };
+    Print_Coords(&point_origin);
 
     return 0;
+}
+
+void Print_Coords(const coordinates* self)
+{
+    printf("(%f , %f)\n", (*self).x, (*self).y);
 }
