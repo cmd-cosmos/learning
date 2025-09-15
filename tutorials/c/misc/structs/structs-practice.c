02bcf2cc377e1d0b7d8e3ddf17bb51c70c1bf9a4
Premanshu Bhagat <premtsapj@gmail.com>
2025-09-15 19:52:31 -0400

    creating arrays of structs

--- tutorials/c/misc/structs/structs-practice.c
+++ tutorials/c/misc/structs/structs-practice.c
@@ -6,6 +6,20 @@
 
 int main(int argc, char* argv[])
 {
+    // struct of arrays
+    typedef struct Point
+    {
+        int x,y;
+    }point_t;
+
+    // create an array of structs 
+    // point_t array path that will store 5 structs here since no size pre-mentioned
+    point_t path[] = {{0,1},{1,2},{2,3},{3,4},{4,5}};
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("(%d, %d)\n", path[i].x, path[i].y);
+    }
     
     return 0;
 }
