fa072aecdf0b001e1e0b7b4044fc83021214f173
Premanshu Bhagat <premtsapj@gmail.com>
2025-09-20 10:52:08 -0400

    foundation for a fueling operation simulation using starship fuel sequence for ship testing

---
+++ fun-programs/python/fueling-operation/main.py
@@ -0,0 +1,17 @@
+### simulating fueling procedure of a rocket
+### fuel LOX and Methane simultaneously
+### pressurize the tank farm
+### chilling the plumbing to cryogenic temperatures
+### start by pressurizing the tanks 
+### proceed to prop load
+### stop prop load at set level
+### perform final tank pressurization to flight pressure
+
+
+# CONSTANTS ---> FLOW RATES, VOLUME, TIME, PRESSURES, TEMPERATURES
+
+
+# ASYNC FUNCTIONS
+
+
+# CONTROL LOOP
