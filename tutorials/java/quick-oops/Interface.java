5fc44196d0dea0b8c9e5048b25511e994c754704
Premanshu Bhagat <premtsapj@gmail.com>
2025-09-26 13:52:41 -0400

    interface keyword for abstract classes

---
+++ tutorials/java/quick-oops/Interface.java
@@ -0,0 +1,15 @@
+public interface Interface {
+    String getName();
+    int getSerialNumber();
+    void displayInfo();
+}
+
+/*
+ * every class that implements the interface needs to use the implements keyword
+ * 
+ * public class Rocket implements Interface{
+ *  constructor
+ * method definitions
+ * }
+ */
+
