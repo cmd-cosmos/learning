ebebb7a69d800163faff6f7f0b3c7be313eb3c57
Premanshu Bhagat <premtsapj@gmail.com>
2025-09-26 12:23:56 -0400

    finished compile time polymorphism using method overloading

--- tutorials/java/quick-oops/Main.java
+++ tutorials/java/quick-oops/Main.java
@@ -1,4 +1,7 @@
 // implementing the Rocket and LaunchPad Class
+// DEMONSTRATES:
+// - POLYMORPHISM
+// - INHERITANCE IN THE SUBCLASSES
 
 public class Main{
     public static void main(String[] args) {
@@ -7,18 +10,24 @@ public class Main{
         LaunchPad launchPad = new LaunchPad();
 
         // INSTANTIATE ROCKETS
-        Rocket starShip = new Rocket("Starship", 1);
-        Rocket falcon = new Rocket("Falcon 9", 2);
         Rocket falconHeavy = new Rocket("Falcon Heavy", 3);
-
+        
         
         // ADDING ROCKET OBJECTS INTO THE LAUNCHPAD ARRAYLIST
+        Rocket starShip = new Rocket("Starship", 1);
         launchPad.addRocket(starShip);
         Stage stage = new Stage("Stage", 38, "Booster");
         launchPad.addRocket(stage);
         Engines engines = new Engines("Veloci", 13, 33, "Raptor III");
         launchPad.addRocket(engines);
-        // launchPad.addRocket(falcon);
+        
+        // POLYMORPHIC BEHAVOR ----> METHOD OVERLOADING
+        System.out.println("Polymorphic behavior:- ");
+        Rocket falcon = new Rocket("Falcon 9", 2);
+        launchPad.addRocket(falcon);
+        launchPad.addRocket("Stage", 1029, "Landing Stage");
+        launchPad.addRocket("Mueller", 21, 8, "Merlin 1-D");        
+        
         // launchPad.addRocket(falconHeavy);
 
         // DISPLAYING THE ROCKETS
