0983cf323da065e1385c64409fe8bd8372eda13d
Premanshu Bhagat <premtsapj@gmail.com>
2025-09-25 23:01:18 -0400

    added a support class to use in tandum with rocket class

---
+++ tutorials/java/quick-oops/LaunchPad.java
@@ -0,0 +1,33 @@
+// class where we instantiate the rockets for diplay or launch
+
+import java.util.ArrayList; // a list for non primitive data types
+
+public class LaunchPad{
+
+    // DECLARE ARRAYLIST TO CONTAIN ROCKET OBJECTS
+    private ArrayList<Rocket> launchPad;
+
+    // CONSTRUCTOR ---> ASSIGNS NEW ARRAYLIST TO PREVIOUSLY DECLARED IDENTIFIER
+    public LaunchPad() 
+    {
+        launchPad = new ArrayList<>();
+    }
+
+    // METHOD TO ADD A ROCKET OBJECT TO THE ARRAYLIST
+    public void addRocket(Rocket rocket)
+    {
+        launchPad.add(rocket);
+    }
+
+    // PRINT METHOD FOR THE LAUNCHPAD LIST
+    public void displayRockets()
+    {
+        for (Rocket rocket : launchPad)
+        {
+            System.out.println("ROCKET: " + rocket.getName());
+            System.out.println("  |----> Serial Number: " + rocket.getSerNum());
+        }
+    }
+
+
+}
