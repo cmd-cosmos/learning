e3dfb97d92cd829be64e755e18c21b5d1d9a6629
Premanshu Bhagat <premtsapj@gmail.com>
2025-09-25 22:45:38 -0400

    created a rocket class to demo encapsulation and basic oops concepts like attributes, methods and constructors

---
+++ tutorials/java/quick-oops/Rocket.java
@@ -0,0 +1,26 @@
+// private ---> access modifier
+// getters and setters allow to access and modify private/protected vals
+// this is an example of encapsulation
+
+public class Rocket{
+    // ATTRIBUTES
+    private String name; // model ---> falcon, starship, SLS, PSLV
+    private int serial_number;
+
+    // CONSTRUCTOR
+    public Rocket(String name, int serial_number)
+    {
+        this.name = name;
+        this.serial_number = serial_number;
+    }
+
+    // GETTER METHODS FOR THE PRIVATE/PROTECTED ATTRIBUTES
+    public String getName()
+    {
+        return name; 
+    }
+    public int getSerNum()
+    {
+        return serial_number; 
+    }
+}
