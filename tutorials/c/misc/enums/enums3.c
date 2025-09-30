23be5b4123cbe5c6ced02104805b1e6d6138e780
Premanshu Bhagat <premtsapj@gmail.com>
2025-09-30 16:05:29 -0400

    added 10% possibility of potential aborts in the sequence

--- tutorials/c/misc/enums/enums3.c
+++ tutorials/c/misc/enums/enums3.c
@@ -11,49 +11,67 @@ typedef enum {
     SECOND_STAGE_IGNITION,
     ORBITAL_INSERTION,
     PAYLOAD_DEPLOY,
+    STAGE_COMPLETE,     // mission completion indicator
     STAGE_ABORT,
-} RocketPhases;
+} RocketPhases_e;
+
+typedef enum{
+    NOMINAL,
+    WARNING,
+    CRITICAL
+} Telemetry_e;
 
 int main()
 {
-    RocketPhases phases;
+    RocketPhases_e phases;
 
     for (int i = STAGE_PREP; i < STAGE_ABORT; i++)
     {
         phases = i;
-        switch (phases)
+        if (rand() % 10 == 0)
         {
-        case STAGE_PREP:
-            printf("All Systems GO\n");
-            sleep(2);
-            break;
-        case STAGE_LAUNCH:
-            printf("Stage Launch Switch ---> Set to Launch\n");
-            sleep(5);
-            break;
-        case FIRST_STAGE_IGNITION:
-            printf("Stage 1 Ignition\n");
-            sleep(10);
-            break;
-        case STAGE_SEPARATION:
-            printf("Hot Staging Maneauver\n");
-            sleep(1);
-            break;
-        case SECOND_STAGE_IGNITION:
-            printf("Stage 2 Ignition\n");
-            sleep(5);
-            break;
-        case ORBITAL_INSERTION:
-            printf("Nominal Orbital Insertion\n");
-            sleep(4);
-            break;
-        case PAYLOAD_DEPLOY:
-            printf("Payload Deployment Confirmed\n");
-            break;
-        case STAGE_ABORT:
-            printf("Abort...\n");
+            printf("Anomaly observed...\n");
+            printf("Proceeding with abort sequence...\n");
+            phases = STAGE_ABORT;
             break;
         }
+        switch (phases)
+        {
+            
+            case STAGE_PREP:
+                printf("All Systems GO\n");
+                sleep(2);
+                break;
+            case STAGE_LAUNCH:
+                printf("Stage Launch Switch ---> Set to Launch\n");
+                sleep(5);
+                break;
+            case FIRST_STAGE_IGNITION:
+                printf("Stage 1 Ignition\n");
+                sleep(10);
+                break;
+            case STAGE_SEPARATION:
+                printf("Hot Staging Maneauver\n");
+                sleep(1);
+                break;
+            case SECOND_STAGE_IGNITION:
+                printf("Stage 2 Ignition\n");
+                sleep(5);
+                break;
+            case ORBITAL_INSERTION:
+                printf("Nominal Orbital Insertion\n");
+                sleep(4);
+                break;
+            case PAYLOAD_DEPLOY:
+                printf("Payload Deployment Confirmed\n");
+                break;
+            case STAGE_COMPLETE:
+                printf("Another Successful Launch and Landing...\n");
+                break;
+            case STAGE_ABORT:
+                printf("Abort...\n");
+                break;
+            }
     }
 
     return 0;
