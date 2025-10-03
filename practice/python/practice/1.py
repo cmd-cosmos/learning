9b238dff8a20a3e71c3c3be2c6acd2fcc351e763
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-03 12:06:53 -0400

    encapsulated read logic in a function

--- practice/python/practice/1.py
+++ practice/python/practice/1.py
@@ -2,6 +2,28 @@
 
 import os
 
+def read_lines(fileName, lineNumber):
+
+    try:
+        file = open(fileName, "r")
+        lines = file.readlines()
+        file.close()
+    except:
+        print("error reading file")
+        exit()    
+
+    print("lines found in file: \n", lines, '\n')
+
+    totalLines = len(lines)
+
+    if (lineNumber > totalLines):
+        print(f"Exceeded total file lines: read {totalLines} lines, requested for line {lineNumber}")
+    else:
+        print("Found in file: ")
+        targetLine = lines[lineNumber - 1].rstrip('\n')
+        print(targetLine)
+
+
 try:
     fileName  = input("File Name: ")
     if not fileName or fileName not in os.curdir:
@@ -18,21 +40,14 @@ except:
     print("using default line ---> fetching line 1\n")
     lineNumber = 1
 
-file = open(fileName, "r")
-lines = file.readlines()
-file.close()
 
-print("lines found in file: \n", lines, '\n')
+read_lines(fileName=fileName, lineNumber=lineNumber)
+
+
+
+#### snippets from the output
 # lines list:
 # ['1. I am Vengeance\n',
 #  '2. I am the Night\n',
 #  '3. I am Batman\n',
 #  '4. Well I am not']
-
-totalLines = len(lines)
-if (lineNumber > totalLines):
-    print(f"Exceeded total file lines: read {totalLines} lines, requested for line {lineNumber}")
-else:
-    print("Found in file: ")
-    targetLine = lines[lineNumber - 1].rstrip('\n')
-    print(targetLine)
