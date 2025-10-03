90f6d255c61d8d90d0a7de7c81d4f71a444aa39a
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-03 13:18:25 -0400

    added some error handling

--- practice/python/practice/2.py
+++ practice/python/practice/2.py
@@ -3,10 +3,25 @@
 import os
 
 os.system("cls")
+os.system("echo running area calculation program")
 
-radius: int  = int(input("Radius: "))
+validFlag = False
+
+while not validFlag:
+    try:
+        radius: int  = int(input("Radius: "))
+        if not radius or radius < 0:
+            raise ValueError("Invlalid Input")
+        validFlag = True
+    except ValueError:
+            os.system("cls")
+            os.system("echo rerunning due to: invalid input")
+    except KeyboardInterrupt:
+         os.system("cls")
+         print("keyboard interrup ---> exiting")
+         exit()
+
 pi: float    = 3.14
-
 area: float  = pi*(radius**2)  
 print("Area of the corresponding circle: {:.2f}".format(area))
 
