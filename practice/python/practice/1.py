27f1dace6dc53c3a6c3c0aa97743b5cb735421d6
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-03 11:54:30 -0400

    dealt with some edge cases

--- practice/python/practice/1.py
+++ practice/python/practice/1.py
@@ -1,20 +1,38 @@
 # read a specific line from a file
 
-fileName  = input("File Name: ")
-lineNumber = input("Line Number: ")
-lineNumber = int(lineNumber)
+import os
+
+try:
+    fileName  = input("File Name: ")
+    if not fileName or fileName not in os.curdir:
+        raise ValueError("Invalid Input")
+except:
+    print("using default input file ---> 1-f.txt\n")
+    fileName = "1-f.txt"   
+
+try:
+    lineNumber = int(input("Line Number: "))
+    if not lineNumber:
+        raise ValueError("Invalid Input")
+except:
+    print("using default line ---> fetching line 1\n")
+    lineNumber = 1
 
 file = open(fileName, "r")
 lines = file.readlines()
 file.close()
 
-print(lines)
+print("lines found in file: \n", lines, '\n')
 # lines list:
 # ['1. I am Vengeance\n',
 #  '2. I am the Night\n',
 #  '3. I am Batman\n',
 #  '4. Well I am not']
 
-targetLine = lines[lineNumber - 1]
-
-print(targetLine)
+totalLines = len(lines)
+if (lineNumber > totalLines):
+    print(f"Exceeded total file lines: read {totalLines} lines, requested for line {lineNumber}")
+else:
+    print("Found in file: ")
+    targetLine = lines[lineNumber - 1].rstrip('\n')
+    print(targetLine)
