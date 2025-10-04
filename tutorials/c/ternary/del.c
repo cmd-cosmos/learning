97208e4ce5c217ac35fe70f98c0ccb554fd93576
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-04 11:04:39 -0400

    added newer implementaion method that works on other languages too

--- tutorials/c/ternary/del.c
+++ tutorials/c/ternary/del.c
@@ -4,12 +4,21 @@ int main()
 {
     int n = 0;
 
+    // old Unix way
     // print 1 file deleted if val files == 1 else pluralize
     printf("%d file%s deleted", n, n == 1 ? "" : "s");
     printf("\n");
 
     n = 1;
     printf("%d file%s deleted", n, n == 1 ? "" : "s");
+    printf("\n");
+
+
+    // newer way
+
+    n = 100;
+    printf(n == 1 ? "%d file deleted" : "%d files deleted", n);
+
 
 
     return 0;
