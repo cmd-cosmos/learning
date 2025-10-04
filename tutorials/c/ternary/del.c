5b4b34c13aa7d9ab9f1fb1c880a6207655f0b4af
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-04 11:10:14 -0400

    functions for the pluralizers

--- tutorials/c/ternary/del.c
+++ tutorials/c/ternary/del.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
 
+void oldPluralizer(int n)
+{
+    printf("%d file%s deleted", n, n == 1 ? "" : "s");
+    printf("\n");
+}
+
+void newPluralizer(int n)
+{
+    printf(n == 1 ? "%d file deleted" : "%d files deleted", n);
+    printf("\n");
+}
+
 int main()
 {
-    int n = 0;
+    oldPluralizer(100);
+    oldPluralizer(1);
+    oldPluralizer(0);
+    printf("\n");
+    newPluralizer(0);
+    newPluralizer(23);
+    newPluralizer(1);
+
 
     // old Unix way
     // print 1 file deleted if val files == 1 else pluralize
-    printf("%d file%s deleted", n, n == 1 ? "" : "s");
-    printf("\n");
-
-    n = 1;
-    printf("%d file%s deleted", n, n == 1 ? "" : "s");
-    printf("\n");
+    // printf("%d file%s deleted", n, n == 1 ? "" : "s");
+    // printf("\n");
 
 
-    // newer way
 
-    n = 100;
-    printf(n == 1 ? "%d file deleted" : "%d files deleted", n);
+    // n = 1;
+    // printf("%d file%s deleted", n, n == 1 ? "" : "s");
+    // printf("\n");
+
+    // // newer way
+
+    // n = 100;
+    // printf(n == 1 ? "%d file deleted" : "%d files deleted", n);
 
 
 
