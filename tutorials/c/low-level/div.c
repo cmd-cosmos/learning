8258d3a3281b1e9cbdfb11f05ebc347102710ccc
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-05 11:38:16 -0400

    some comments to explaing how the bitshift works for division by 2

--- tutorials/c/low-level/div.c
+++ tutorials/c/low-level/div.c
@@ -10,12 +10,16 @@ int main()
     double res, res_opt;
 
     a = 8;
-    res = a / 2;
+    res = a / 2.0;
     printf("%f\n", res);
 
+    // this bitwise operation only behaves predictably for positive ints
     b = 8;
     res_opt = b >> 1; // bitwise shift for division by 2
     printf("%f\n", res_opt);
+
+    // 8 ----> 0000 1000 ---> div by 2 get 4 ----> 1 right shift for the bits will lead to the result
+    // 4 ----> 0000 0100 <---- 1 bitwise shift to the right
 
     return 0;
 }
