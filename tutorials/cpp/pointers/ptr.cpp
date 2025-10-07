2fbdd087a27ec14df00ecb02b1c78236d5f6aef0
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-06 20:05:13 -0400

    added some  pointer arithmatic and deref basics

--- tutorials/cpp/pointers/ptr.cpp
+++ tutorials/cpp/pointers/ptr.cpp
@@ -8,6 +8,7 @@ void print(const std::string& brief,const auto& var)
 
 int main()
 {
+    // ptr and pointee must be of same type
     int x = 5;
     int *ptr= &x;
 
@@ -15,6 +16,26 @@ int main()
     print("&ptr = ",&ptr);  // addr of the ptr var
     print("ptr = ",ptr);    // val assigned to ptr var
     print("&x = ", &x);     // same as ptr assignment
+
+    // deref
+    print("*ptr = ", *ptr);
+
+    // deref + modify
+    *ptr += 10;
+    print("modified x = ", x);
+    print("modified ptr deref = ", *ptr);
+
+    // ptr arithmatic
+    std::string array[5] = {
+        "Batman", "Alfred", "Robin", "Joker", "Scarecrow"
+    };
+    print("&array[0] = ", array);
+    print("array = ", array);
+
+    print("array[0] = ", array[0]);
+    print("*array  = ", *array);
+
+    print("*(array + 1) = ", *(array + 1));
 
     return 0;
 }
