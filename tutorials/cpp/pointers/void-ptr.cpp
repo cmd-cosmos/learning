be0a7b74ad88bd5a5197a4799a0f670508e18a6d
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-08 23:35:25 -0400

    started working on void pointers

--- tutorials/cpp/pointers/void-ptr.cpp
+++ tutorials/cpp/pointers/void-ptr.cpp
@@ -1,8 +1,34 @@
 // ref:: https://www.youtube.com/watch?v=kiUGf_Z08RQ
+// allows to cast to any data type
 
 #include <iostream>
+#include <string>
 
+void print(const std::string& brief,const auto& var)
+{
+    std::cout << "Logger: " << brief << var << std::endl;
+}
 
+void print(void* ptr, char type)
+{
+    switch (type)
+    {
+    case 'i':
+        // handles int pointers
+        break;
+    case 'c':
+        // char ptr handler
+        break;
+    case 'f':
+        // float ptr handler
+        break;
+    case 'd':
+        // double ptr handler
+        break;
+    default:
+        break;
+    }
+}
 
 int main()
 {
