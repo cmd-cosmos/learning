2c7171bdbcb30defdfb64212d9db55eb19be8b34
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-15 21:42:18 -0400

    basics 1

---
+++ tutorials/js/syntax-misc/var.js
@@ -0,0 +1,17 @@
+// variables in js
+
+var text = "Batman";
+
+let var2 = "Joker";
+
+const var3 = "Time";
+
+function add10(x) 
+{
+    var y = 10;
+    console.log(y);
+    console.log(x + y);
+}
+
+
+add10(5);
