40c3889f18c83f031995865bde1da7b691e5b1e7
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-21 14:06:31 -0400

    dict keys with unique float vals supported

--- tutorials/python/language-details/dict-property.py
+++ tutorials/python/language-details/dict-property.py
@@ -7,14 +7,21 @@ d = {
 }
 
 # 1 and 1.0 treated the same despite being different types as they equate to the same int value
+print(d)
 print(f"d[1]   = {d[1]}")
 print(f"d[1.0] = {d[1.0]}")
-
 # Output:
+# {1: 'Alfred'}
 # d[1]   = Alfred
 # d[1.0] = Alfred
+print("-"*30)
+# different float vals act as separate keys
+d2 = {
+    1 : "Batman",
+    1.23 : "Alfred"
+}
+print(d2)
+print(f"d2[1]   = {d2[1]}")
+print(f"d2[1.23] = {d2[1.23]}")
 
-
-print(d)
-# {1: 'Alfred'}
 
