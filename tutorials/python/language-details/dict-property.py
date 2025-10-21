c82837db1a772b1ab129a49a714e95086a8e72b0
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-21 14:09:29 -0400

    float key casted to string 1 ---> 1.0 ---> '1.0' treated as unique

--- tutorials/python/language-details/dict-property.py
+++ tutorials/python/language-details/dict-property.py
@@ -2,19 +2,21 @@
 # pylint: skip-file
 
 d = {
-    1   : "Batman",
-    1.0 : "Alfred"
+    1     : "Batman",
+    1.0   : "Alfred",
+    '1.0' : "Joker"
 }
 
 # 1 and 1.0 treated the same despite being different types as they equate to the same int value
 print(d)
-print(f"d[1]   = {d[1]}")
-print(f"d[1.0] = {d[1.0]}")
+print(f"d[1]     = {d[1]}")
+print(f"d[1.0]   = {d[1.0]}")
+print(f"d['1.0'] = {d['1.0']}") # when 1.0 is cast to str it serves as a unique key
 # Output:
-# {1: 'Alfred'}
-# d[1]   = Alfred
-# d[1.0] = Alfred
-print("-"*30)
+# {1: 'Alfred', '1.0': 'Joker'}
+# d[1]     = Alfred
+# d[1.0]   = Alfred
+# d['1.0'] = Joker
 # different float vals act as separate keys
 d2 = {
     1 : "Batman",
