b020150bf66098e7be2e1d66bafd6bdee3899f71
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-24 12:44:27 -0400

    working on class structure for evolving oses

---
+++ tutorials/python/syntax-misc/inherit.py
@@ -0,0 +1,46 @@
+#pylint: skip-file
+
+import asyncio
+
+class Job:
+    def __init__(self, id, waitTime) -> None:
+        self.id = id
+        self.waitTime = waitTime        
+
+class Multiprogramming:
+    def __init__(self, num_jobs, job_list) -> None:
+        self.num_jobs = num_jobs
+        self.job_list = job_list
+    
+    def submitJobs(self):
+        for job in self.job_list:
+            print("Submitted: ", job)
+    
+    async def runMultiProgSys(self):
+        for job in self.job_list:
+            print("running job 1")
+        
+
+class Multitasking(Multiprogramming):
+    def __init__(self, num_jobs, job_list, user) -> None:
+        super().__init__(num_jobs, job_list)
+        self.user = user
+
+class TimeSharing(Multiprogramming):
+    def __init__(self, num_jobs, job_list, users) -> None:
+        super().__init__(num_jobs, job_list)
+        self.users = users
+
+    def timeAlloc(self):
+        for user in self.users:
+            print("Allocating time to user: ", user)
+
+
+j1 = Job(id=1, waitTime=2)
+j2 = Job(id=2, waitTime=5)
+j3 = Job(id=3, waitTime=1)
+j4 = Job(id=4, waitTime=3)
+j5 = Job(id=5, waitTime=10)
+
+job_list = [j1, j2, j3, j4, j5]
+
