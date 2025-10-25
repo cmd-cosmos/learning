c5978a4bb9a5bcc4f36e6fa8f36386c0584a3f6e
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-25 14:02:24 -0400

    finished PCB class, working on Process class

--- tutorials/python/oops/processControl.py
+++ tutorials/python/oops/processControl.py
@@ -7,8 +7,7 @@
 
 
 class PCB:
-    def __init__(self) -> None:
-        state_map = {
+    state_map = {
             1 : "NEW",
             2 : "READY",
             3 : "RUNNING",
@@ -16,13 +15,36 @@ class PCB:
             5 : "TERMINATED",
         }
         
+    def __init__(self, pid) -> None:
+        self.pid = pid
+        self.state = 1
+
+    def setState(self, newState: int):
+        if newState in self.state_map:
+            print(f"PID: {self.pid} | TRANSITION: {self.state_map[self.state]} --> {self.state_map[newState]}")
+            self.state =newState
+        else:
+            print(f"PID: {self.pid} | Invalid Transition Request")
 
 class Process:
+    pidCounter = 0
+
     def __init__(self) -> None:
-        pass
+        self.pcb = None
 
     def createProcess(self):
-        pass
+        Process.pidCounter += 1
+        self.pcb = PCB(Process.pidCounter)
+        print(f"Process: {self.pcb.pid} | Created State: {PCB.state_map[self.pcb.state]}")
+        self.admit()
+
+    def admit(self):
+        self.pcb.setState(2)
+
+    def dispatch(self):
+        self.pcb.setState(3) if self.pcb.state == 2 else print("Cannot dispatch process until ")
+    
+
 
 
 
