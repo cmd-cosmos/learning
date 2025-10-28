d067dc75835d56278d7d9f369c593469199ac2ac
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-28 16:20:00 -0400

    added another example to check endiannes

--- tutorials/c/advanced-c/endianness/endian2.c
+++ tutorials/c/advanced-c/endianness/endian2.c
@@ -22,6 +22,10 @@ int main(int argc, char* argv[])
     unsigned int x = 1;  // 0x00000001
     char* c = (char*)&x; // inspect the first byte
 
+    for (int i = 0; i < sizeof(x); i++)
+    {
+        printf("Byte %d: 0x%02x\n", i, c[i]);
+    }
 
     if (*c)
     {
@@ -31,6 +35,31 @@ int main(int argc, char* argv[])
         printf("big endian\n");
     }
 
+    unsigned int y = 0x12345678;
+    
+    // 0x78 ---> 7*16^1 + 8*16^0 = 7*16 + 8 = 120 in decimal
+    if (*(char*)&y == 0x78 && *(char*)&y == 120)         
+    {
+        printf("\nlittle endian\n");
+        printf("val at first byte: %02x", *(char*)&y);
+    }
+    else
+    {
+        printf("\nbig endian\n");
+    }
+
     return 0;
 }
+
+/*
+
+output:
+
+Byte 0: 0x01 <---- LSB stored in byte 0
+Byte 1: 0x00
+Byte 2: 0x00
+Byte 3: 0x00
+little endian
+
+*/
 
