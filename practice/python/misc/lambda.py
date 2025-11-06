2f4027020a3f2fc1029a8ccfab0db675cb64a7a9
Premanshu Bhagat <premtsapj@gmail.com>
2025-11-05 22:39:58 -0500

    added some practice questions

--- practice/python/misc/lambda.py
+++ practice/python/misc/lambda.py
@@ -1,5 +1,7 @@
 #pylint: disable=C0114
+#pylint: disable=C0116
 #pylint: disable=C0121
+#pylint: disable=C0301
 #pylint: disable=C0305
 
 # 1. apply a 10% tax and round the res
@@ -28,4 +30,18 @@ characters = [
 ]
 sorted_characters = sorted(characters, key=lambda x: x["power"])
 print(f"characters by power: {sorted_characters}")
+
+# 4. combine lambdas and higher order functions -> add 5% tax and filter below 100 after tax and round results
+prices = [50, 80, 120, 200]
+calcs = list(filter(lambda x: x < 100,(map(lambda x: round(x + 0.05*x), prices))))
+print(calcs)
+
+# 5. using lambdas in real functions
+def make_power(n):
+    return lambda x: x**n
+square = make_power(2)
+cube = make_power(3)
+
+print(square(4))
+print(cube(2))
 
