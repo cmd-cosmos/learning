8f54f729c3e141778409907b099970fbb4008481
Premanshu Bhagat <premtsapj@gmail.com>
2025-11-05 21:51:14 -0500

    added lambda keys to sort dicts

--- tutorials/python/syntax-misc/lambda.py
+++ tutorials/python/syntax-misc/lambda.py
@@ -20,3 +20,17 @@ lst = [1,2,3,4,5,6,7,8,9,10]
 evens = list(filter(lambda x: (x % 2 == 0), lst))
 print(f"evens : {evens}")
 
+# lambdas as key for sorted function
+l = ["batman", "joker", "riddler", "scarecrow", "robin"]
+sorted_words = sorted(l, key=lambda x: len(x))
+print(sorted_words)
+
+d = {
+    "batman" : 1000,
+    "joker" : 170,
+    "robin" : 20,
+    "scarecrow" : 65,
+    "riddler" : 34,
+}
+sorrted_dict = sorted(d, key=lambda x: d[x])
+print(sorrted_dict)
