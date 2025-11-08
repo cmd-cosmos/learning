93ef2c4e48100488419464c70d8e182b7e16b449
Premanshu Bhagat <premtsapj@gmail.com>
2025-11-08 10:25:13 -0500

    order of decorators

--- tutorials/python/language-details/decorators/decor3.py
+++ tutorials/python/language-details/decorators/decor3.py
@@ -2,14 +2,26 @@
 
 def uppercase(func):
     def wrapper(*args, **kwargs):
-        print("Entering decorator funciton...")
+        print("Entering uppercase decor...")
         temp = func(*args, **kwargs)
         res = temp.upper()
         return res
     return wrapper
 
+def splitter_decor(func):
+    def wrapper(*args, **kwargs):
+        print("Entering splitter wrapper...")
+        temp = func(*args, **kwargs)
+        res = temp.split(',')
+        return res
+    return wrapper
 
-@uppercase
+# this order will give an error as uppercase decor will wrap the splitter decor so we will first get a list which it will try to capitalize.
+# @uppercase
+# @splitter_decor
+
+@splitter_decor     # split the capitalized string
+@uppercase          # capitalize first
 def greet(name):
     return f"Hello, {name}"
 
