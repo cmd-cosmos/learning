a101456c6a8e99061e12bd98e1a2b35792613fe7
Premanshu Bhagat <premtsapj@gmail.com>
2025-11-14 12:39:17 -0500

    luhns algo for cc validation.

---
+++ fun-programs/cpp/credit-card-validation/main.cpp
@@ -0,0 +1,10 @@
+// implementing Luhn algorithm for cc validation
+// double every 2nd digit from right to left
+// if doubled num is 2 digits --> split them
+// add all single digits from step 1
+// add all odd numbered digits from right to left
+// sum results from steps 2 & 3
+// if prev step is divisible by 10, num is valid
+
+
+
