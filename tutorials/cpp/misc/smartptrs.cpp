49b0675953a043374a3bc20fcaaf1262effbc179
Premanshu Bhagat <premtsapj@gmail.com>
2025-11-15 11:16:20 -0500

    scope changes for shared ptrs and added weak pointers

--- tutorials/cpp/misc/smartptrs.cpp
+++ tutorials/cpp/misc/smartptrs.cpp
@@ -29,18 +29,35 @@ int main()
     }
 
     // shared pointers
-    std::shared_ptr<int> p1 = std::make_shared<int>(100);
-    std::shared_ptr<int> p2 = p1; // shared pointers
-    
-    std::cout << p1.use_count() << '\n'; // total ptrs sharing similar ownership
-
-    std::shared_ptr<Foo> o1 = std::make_shared<Foo>();
-    std::cout << o1.use_count() << '\n';
     {
-        std::shared_ptr<Foo> o2 = o1;
+        std::shared_ptr<int> p1 = std::make_shared<int>(100);
+        std::shared_ptr<int> p2 = p1; // shared pointers
+        
+        std::cout << p1.use_count() << '\n'; // total ptrs sharing similar ownership
+        
+        std::shared_ptr<Foo> o1 = std::make_shared<Foo>();
         std::cout << o1.use_count() << '\n';
+        {
+            std::shared_ptr<Foo> o2 = o1;
+            std::cout << o1.use_count() << '\n';
+        }
+        std::cout << o1.use_count() << '\n';
+    
     }
-    std::cout << o1.use_count() << '\n';
+    
+    // weak pointers
+    std::shared_ptr<int> sp = std::make_shared<int>(1);
+    std::weak_ptr<int>   wp = sp;
+
+    if (auto locked = wp.lock()){
+        std::cout << *locked << '\n';
+    }
+
+    sp.reset();
+
+    if (wp.expired()){
+        std::cout << "obj killed\n";
+    }
 
     return 0;
 }
