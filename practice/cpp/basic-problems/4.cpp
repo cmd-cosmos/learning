8c7f3d3c6ddc1a9c4a8cd4bae407d63fdd4b4815
Premanshu Bhagat <premtsapj@gmail.com>
2025-11-18 20:58:07 -0500

    leap year check using ternary operators.

--- practice/cpp/basic-problems/4.cpp
+++ practice/cpp/basic-problems/4.cpp
@@ -16,6 +16,9 @@ int main()
     {
         std::cout << "Not a Leap Year.\n";
     }
+
+    // using ternary operators
+    (year % 4 == 0 && year % 100 != 0) ? std::cout << "Leap Year." : ((year % 400 == 0) ? std::cout << "Leap Year" : std::cout << "Not a Leap Year.");
     
     return EXIT_SUCCESS;
 }
