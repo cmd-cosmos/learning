02cdbecb9a62d79de3a49753f37928e599bad129
Premanshu Bhagat <premtsapj@gmail.com>
2025-11-18 20:51:05 -0500

    basic leap year check using logical operators.

---
+++ practice/cpp/basic-problems/4.cpp
@@ -0,0 +1,21 @@
+// leap year check
+
+#include <iostream>
+
+int main()
+{
+    int year;
+    std::cout << "Enter Year: "; // insert the text in the cout stream
+    std::cin >> year; // year will extract a val from the input stream
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    {
+        std::cout << "Leap Year.\n";
+    }
+    else
+    {
+        std::cout << "Not a Leap Year.\n";
+    }
+    
+    return EXIT_SUCCESS;
+}
