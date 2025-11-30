89d6d704c2c993bacd1a2f8df99d247268154693
Premanshu Bhagat <premtsapj@gmail.com>
2025-11-30 12:21:18 -0500

    finished a basic to do list.

--- tutorials/js/misc-misc/tasks.js
+++ tutorials/js/misc-misc/tasks.js
@@ -1,5 +1,4 @@
 const readline = require("readline");
-const { text } = require("stream/consumers");
 
 const r1 = readline.createInterface({
     input: process.stdin,
@@ -23,11 +22,40 @@ r1.question("Choose Option: ", handleRequest);
 function handleRequest(option){
     switch (option){
         case "1":
-            r1.question("Enter Question: ", task => {
+            r1.question("Enter Task: ", task => {
                 tasks.push({text : task, done : false});
                 console.log("Task Added.");
                 displayMenu();
             });
             break;
+        case "2":
+            console.log("-------- Task List ---------");
+            tasks.forEach((t, i) =>
+            console.log(`${i}. [${t.done ? "DONE" : "NOT DONE"}] ${t.text}`));
+            displayMenu();
+            break;
+        case "3":
+            r1.question("Enter task id: ", n => {
+                if (tasks[n]) {
+                    tasks[n].done = true;
+                    console.log("Marked Done");
+                }
+                displayMenu();
+            });
+            break;
+        case "4":
+            r1.question("Enter task id: ", n => {
+                tasks.splice(n, 1);
+                console.log("Deleted Task");
+                displayMenu();
+            });
+            break;
+        case "5":
+            r1.close();
+            break;
+        default:
+            displayMenu();
     }
 }
+
+displayMenu();
