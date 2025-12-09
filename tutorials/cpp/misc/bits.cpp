f5bf7933bd6b8c70158887f6045e030910563e38
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-08 19:01:19 -0500

    setting a bit using or

--- tutorials/cpp/misc/bits.cpp
+++ tutorials/cpp/misc/bits.cpp
@@ -12,5 +12,13 @@ int main()
     }
     // bit 7 is the MSB and bit 0 is the LSB
 
+    bits = bits | (1 << 0); // set the first bit i.e bit 0
+   
+    std::cout << "\nAfter set bit 0: \n";
+    for (int i = 0; i < 8; i++)
+    {
+        std::cout << "bit " << i << ": " << ((bits >> i) & 1) << '\n';
+    }
+
     return EXIT_FAILURE;
 }
