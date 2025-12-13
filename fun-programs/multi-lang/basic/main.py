cc4900aae2d73d6cb7d96ec5b7db95acd6811457
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-13 00:28:36 -0500

    basic python script passing inputs to cpp exe.

---
+++ fun-programs/multi-lang/basic/main.py
@@ -0,0 +1,9 @@
+#pylint: skip-file
+#type: ignore
+
+import subprocess
+
+process = subprocess.run(["multiply.exe"], input="100 2000", text=True, capture_output=True)
+
+res = int(process.stdout.strip())
+print("C++ routine returned: ", res)
