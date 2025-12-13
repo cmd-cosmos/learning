d26d61e6368c4df7b17ad86de8277ff904ae7d8f
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-13 00:29:11 -0500

    modified and added std::err prints to add debig steps

--- fun-programs/multi-lang/basic/multiply.cpp
+++ fun-programs/multi-lang/basic/multiply.cpp
@@ -2,9 +2,12 @@
 
 int main()
 {
+    std::cerr << "\n------- Entered C++ Routine -------\n";
     int a, b;
     std::cin >> a >> b;
+    std::cerr << "Multiplying: " << a << " and " << b << '\n';
     std::cout << a * b;
+    std::cerr << "------- Exiting C++ Routine -------\n";
 
     return 0;
 }
