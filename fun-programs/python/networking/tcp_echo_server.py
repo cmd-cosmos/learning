3c6c9c8e517ac245250b02ffd6cf7847111bc271
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-13 00:54:50 -0500

    basic tcp echo server

---
+++ fun-programs/python/networking/tcp_echo_server.py
@@ -0,0 +1,25 @@
+#pylint:skip-file
+#type:ignore
+
+import socket
+
+HOST = "127.0.0.1"
+PORT = 5000
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(HOST, PORT)
+    s.listen()
+    print(f"listening on {HOST}: {PORT}")
+
+    conn, addr = s.accept()
+
+    with conn:
+        print("connected by: ", addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+
+    
+    
