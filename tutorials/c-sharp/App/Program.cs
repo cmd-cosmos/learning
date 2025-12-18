129cd0ae2af7b0947f606b86790ad1d767b5be64
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-18 14:08:25 -0500

    basic sum program in C sharp

---
+++ tutorials/c-sharp/App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Greet();
+        SumNumbers();
+    }
+
+    static void Greet()
+    {
+        Console.WriteLine(new string(' ',30) + "Greetings" + new string(' ',30));
+        Console.WriteLine(new string('*',70));
+    }
+
+    static void SumNumbers()
+    {
+        Console.Write("Enter Num 1: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter Num 2: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        int sum = a + b;
+        Console.WriteLine("Sum: " + sum);
+    }
+}
