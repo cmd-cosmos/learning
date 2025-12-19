330adc6d5ed328b63f366755ef1e7d47c560621d
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-19 12:38:45 -0500

    added menu to include multiple routines

--- tutorials/c-sharp/App/Program.cs
+++ tutorials/c-sharp/App/Program.cs
@@ -4,8 +4,27 @@ class Program
 {
     static void Main()
     {
-        Greet();
-        SumNumbers();
+        Console.WriteLine(new string(' ', 30) + "Program List" + new string(' ', 30));
+        Console.WriteLine("1. Greet Me");
+        Console.WriteLine("2. Add Nums");
+        Console.WriteLine(new string('*', 70));
+        Console.Write("What program do you want to run: ");
+        int mode = Convert.ToInt32(Console.ReadLine());
+
+        switch (mode)
+        {
+            case 1:
+                Console.WriteLine(new string('*', 70));
+                Greet();
+                break;
+            case 2:
+                Console.WriteLine(new string('*', 70));
+                SumNumbers();
+                break;
+            default:
+                Console.WriteLine("Invalid Input.");
+                break;       
+        }
     }
 
     static void Greet()
