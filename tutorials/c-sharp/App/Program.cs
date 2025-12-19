5bca86b274b75116da0ff0b6edaac32ef5341a34
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-19 13:59:40 -0500

    added a basic text analyzer

--- tutorials/c-sharp/App/Program.cs
+++ tutorials/c-sharp/App/Program.cs
@@ -10,7 +10,8 @@ class Program
             Console.WriteLine(new string(' ', 30) + "Program List" + new string(' ', 30));
             Console.WriteLine("1. Greet Me");
             Console.WriteLine("2. Add Nums");
-            Console.WriteLine("3. Exit");
+            Console.WriteLine("3. Text Analyzer");
+            Console.WriteLine("4. Exit");
             Console.WriteLine(new string('*', 70));
             Console.Write("What program do you want to run: ");
             int mode = Convert.ToInt32(Console.ReadLine());
@@ -26,6 +27,10 @@ class Program
                     SumNumbers();
                     break;
                 case 3:
+                    Console.WriteLine(new string('*', 70));
+                    TextAnalyzer();
+                    break;
+                case 4:
                     Console.WriteLine(new string('*', 70));
                     Console.WriteLine("Exiting program");
                     return;
@@ -53,4 +58,39 @@ class Program
         int sum = a + b;
         Console.WriteLine("Sum: " + sum);
     }
+
+    static void TextAnalyzer()
+    {
+        Console.Write("Enter Text: ");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+        string inp = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+        int letters = 0;
+        int digits  = 0;
+        int spaces  = 0;
+
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+        foreach (char c in inp)
+        {
+            if (char.IsLetter(c))
+            {
+                letters++;
+            }
+            else if (char.IsDigit(c))
+            {
+                digits++;
+            }
+            else if (char.IsWhiteSpace(c))
+            {
+                spaces++;
+            }
+        }
+        Console.WriteLine("\nResult:");
+        Console.WriteLine("Total Chars: " + inp.Length);
+        Console.WriteLine("Letters :" + letters);
+        Console.WriteLine("Digits  :" + digits);
+        Console.WriteLine("Spaces  :" + spaces);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+    } 
 }
