4a24e6006ba7e2e3e92ea78ba8870fda3576acc4
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-20 13:51:45 -0500

    added safe allocation function which compares size input to SIZE_MAX macro in C

--- tutorials/c/misc/unsafe/int_overflow.c
+++ tutorials/c/misc/unsafe/int_overflow.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
 void unsafe_alloc(size_t count)
 {
@@ -13,9 +14,30 @@ void unsafe_alloc(size_t count)
     free(arr);
 }
 
+void safe_alloc(size_t count)
+{
+    if (count > SIZE_MAX / sizeof(int))
+    {
+        printf("Validation failed in comparision to SIZE_MAX.\n");
+        return;
+    }
+    printf("Validation Successful => Allocating Memory\n");
+        
+    int* arr = malloc(count * sizeof(int)); // chances to overflow
+    if (!arr)
+    {
+        printf("Overflow --> Not Allocated\n");
+        return;
+    }
+    arr[count-1] = 100;
+    free(arr);
+}
+
+
 int main()
 {
-    unsafe_alloc(0xFFFFFFFFFFF);
-
+    // unsafe_alloc(0xFFFFFFFFFFF);
+    safe_alloc(0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFF);
+    safe_alloc(0xFFFFFFFFFFFFF);
     return 0;
 }
