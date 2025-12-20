db5ec9e8155ebf9b267ee7d0503939ff21419714
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-20 13:03:28 -0500

    some formatting changes

--- tutorials/c/misc/unsafe/use_after_free.c
+++ tutorials/c/misc/unsafe/use_after_free.c
@@ -5,6 +5,7 @@
 
 void safeUse()
 {
+    printf("executing safe routine:\n");
     int *p = malloc(sizeof(int));
     if (!p) return;
 
@@ -13,22 +14,26 @@ void safeUse()
 
     free(p);
     p = NULL; // poisoning the malloc ptr
+    printf("exiting safe routine\n");
 
 }
 
 void unsafe()
 {
+    printf("executing unsafe routine:\n");
     int *p = malloc(sizeof(int));
     *p = 1000;
 
     free(p);
     printf("%d\n", *p); // use after 
     // memory leak: -2038919296
+    printf("exiting unsafe routine\n");
 }
 
 int main()
 {
     safeUse();
+    printf('\n');
     unsafe();
 
     return 0;    
