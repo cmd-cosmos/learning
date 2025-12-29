229632debc1a7f75262dde437cc3adbf1fa880d7
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-28 19:46:44 -0500

    template literals and objects

---
+++ tutorials/js/misc-misc/basics.js
@@ -0,0 +1,13 @@
+let score = 100;
+
+console.log(`score = ${score}`)
+
+let user = {
+    name : "batman",
+    age  : 22,
+    isInGotham : true
+};
+
+console.log(user.name)
+console.log(user.age)
+console.log(user.isInGotham)
