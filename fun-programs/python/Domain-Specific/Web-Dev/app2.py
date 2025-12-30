599404c9fd9c1863a3132eee9147df3fd08eece2
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-30 12:49:06 -0500

    multi page web server

--- fun-programs/python/Domain-Specific/Web-Dev/app2.py
+++ fun-programs/python/Domain-Specific/Web-Dev/app2.py
@@ -8,5 +8,13 @@ app = Flask(__name__)
 def home():
     return "I am Vengeance"
 
+@app.route("/about")
+def about():
+    return "this is the about page"
+
+@app.route("/user/<name>")
+def user(name):
+    return f"I am {name}"
+
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
