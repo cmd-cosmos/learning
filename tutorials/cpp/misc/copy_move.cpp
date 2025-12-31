23786d799c6db31d05156c83d5970d8f0c7c6287
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-31 16:00:16 -0500

    constructing, copying and moving objects

--- tutorials/cpp/misc/copy_move.cpp
+++ tutorials/cpp/misc/copy_move.cpp
@@ -4,18 +4,24 @@
 struct Box{
     std::vector<int> data;
 
-    Box() : data(1'000'000, 21) {}
+    Box() : data(1'000'000, 21) {
+        std::cout << "constructing obj\n";
+    }
 
     Box(const Box&) {
-        std::cout << "copy\n";
+        std::cout << "copy obj\n";
     }
 
     Box(Box&&) noexcept {
-        std::cout << "move\n";
+        std::cout << "move obj\n";
     }
 };
 
 int main(void)
 {
+    Box a;
+    Box b = a;
+    Box c = std::move(a);
+
     return EXIT_SUCCESS;
 }
