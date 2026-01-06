d270805e6f71ef7981ef432ddfaa4f137aca95f6
Premanshu Bhagat <premtsapj@gmail.com>
2026-01-06 17:44:28 -0500

    exception handling in case of hardware failure

---
+++ fun-programs/cpp/misc/motor_failure.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdexcept>
+
+class Motor {
+    public:
+        void setRPM(double rpm) {
+            if (rpm > 3500) {
+                throw std::runtime_error("MOTOR OVERSPEEDING\n");
+            }
+            else {
+                std::cout << "Motor RPM: " << rpm << " RPM";
+            }
+        }
+};
+
+int main(void)
+{
+    Motor m;
+    try
+    {
+        m.setRPM(1000);
+    }
+    catch(const std::runtime_error& e)
+    {
+        std::cout << "MOTOR ERROR: " << e.what() << '\n';
+    }
+    
+    std::cout << "\nSetting motor speed to 5000 RPM\n";
+
+    try
+    {
+        m.setRPM(5000);
+    }
+    catch(const std::runtime_error& e)
+    {
+        std::cerr << "MOTOR ERROR: " << e.what() << '\n';
+    }
+    
+
+    return EXIT_SUCCESS;
+}
