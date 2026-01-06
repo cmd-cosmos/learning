4450e09901a1a66528f53557238e236817957628
Premanshu Bhagat <premtsapj@gmail.com>
2026-01-06 13:41:06 -0500

    moving ownership in case of unique ptrs

--- tutorials/cpp/misc/unique.cpp
+++ tutorials/cpp/misc/unique.cpp
@@ -5,5 +5,9 @@ int main(void) {
     auto  p = std::make_unique<int>(100);
     std::cout << *p << "\n";
 
+    // move ownership in case of unique ptrs
+    auto p2 = std::move(p);
+    std::cout << *p2 << "\n";
+
     return EXIT_SUCCESS;
 }
