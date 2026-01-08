add1cf4bc1cbaf98a6924a233157aa90f53cb3eb
Premanshu Bhagat <premtsapj@gmail.com>
2026-01-07 22:39:35 -0500

    added argparse to take num input instead of input statement

--- fun-programs/python/misc/pretty_cli.py
+++ fun-programs/python/misc/pretty_cli.py
@@ -3,17 +3,25 @@
 
 from rich import print
 from rich.table import Table
+import argparse
 
-table = Table(title="Mult Table")
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--val", type=int, required=True, help="Value for mult table")
+    args = parser.parse_args()
 
-table.add_column("a", justify="right")
-table.add_column("b", justify="right")
-table.add_column("result", justify="right")
+    inp = args.val
+    table = Table(title="Mult Table")
 
-inp = int(input("Enter num: "))
-print('\n')
+    table.add_column("a", justify="right")
+    table.add_column("b", justify="right")
+    table.add_column("result", justify="right")
 
-for i in range(1, 11):
-    table.add_row(str(inp), str(i), str(inp * i))
+    # inp = int(input("Enter num: "))
+    print('\n')
 
-print(table)
+    for i in range(1, 11):
+        table.add_row(str(inp), str(i), str(inp * i))
+
+    print(table)
