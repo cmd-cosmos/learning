bffdefec7ae379ac6d1337f09bdae692e986dcc0
Premanshu Bhagat <premtsapj@gmail.com>
2026-01-09 13:12:02 -0500

    integrated colorama to print text in green

--- fun-programs/python/misc/hacker_text.py
+++ fun-programs/python/misc/hacker_text.py
@@ -4,11 +4,14 @@
 import random
 import time
 import string
+from colorama import Fore, Style, init
+
+init()
 
 chars = string.ascii_letters + string.digits + "@$#%"
 line_count = 0
 while line_count < 100:
     line = "".join(random.choice(chars) for _ in range(80))
-    print(line)
+    print(Fore.GREEN + line + Style.RESET_ALL)
     line_count += 1
     time.sleep(0.05)
