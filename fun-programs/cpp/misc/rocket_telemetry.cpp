5339cbdef32b07cd79c7eb817b992e50b1dc9c1e
Premanshu Bhagat <premtsapj@gmail.com>
2026-01-12 20:50:43 -0500

    telemetry packet producer function

--- fun-programs/cpp/misc/rocket_telemetry.cpp
+++ fun-programs/cpp/misc/rocket_telemetry.cpp
@@ -29,6 +29,44 @@ std::condition_variable bufferNotFull;
 
 std::atomic<bool> missionActive(true);
 
+/**
+ * @brief Producer function that puts data into the buffer
+ * 
+ * @return void
+ */
+void sensorProd()
+{
+    int packetCounter = 0;
+
+    while(missionActive)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+
+        TelemetryPacket_t packet;
+        packet.packetID     = packetCounter++;
+        packet.altitude     = 1000.00 + packetCounter * 50;
+        packet.velocity     = 200.00 + packetCounter * 3;
+        packet.temperature  = 20.0 + packetCounter * 0.1;
+        
+        std::unique_lock<std::mutex> lock(bufferMutex);
+
+        bufferNotFull.wait(lock, [] {
+            return telemetryBuffer.size() < MAX_BUFFER_SIZE || !missionActive;
+        });
+
+        if (!missionActive) {
+            break;
+        }
+
+        telemetryBuffer.push(packet);
+        std::cout << "[PRODUCER] produced: " << packet.packetID << '\n';
+        
+        lock.unlock();
+        bufferNotEmpty.notify_one();
+    }
+    std::cout << "[PRODUCER] stopped telemetry generation\n";
+}
+
 int main(void)
 {
 
