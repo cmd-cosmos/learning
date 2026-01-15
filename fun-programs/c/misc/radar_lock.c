e27aca0a4a59d8501f933a5d23b6349831d58608
Premanshu Bhagat <premtsapj@gmail.com>
2026-01-14 21:46:38 -0500

    added a basic radar update function

--- fun-programs/c/misc/radar_lock.c
+++ fun-programs/c/misc/radar_lock.c
@@ -12,6 +12,56 @@ typedef enum
 #define LOCK_CONFIRM_TICKS (3U)
 #define LOCK_LOSS_TICKS    (4U)
 
+static radar_state_t radar_state = RADAR_STATE_SEARCH;
+static uint32_t lock_timer = 0U;
+static uint32_t loss_timer = 0U;
+
+static void radar_update(bool target_detected)
+{
+    switch (radar_state)
+    {
+    case RADAR_STATE_SEARCH:
+        if (target_detected == true)
+        {
+            radar_state = RADAR_STATE_TRACK;
+            lock_timer = 0U;
+        }
+        break;
+    case RADAR_STATE_TRACK:
+        if (target_detected == true)
+        {
+            lock_timer++;
+            if (lock_timer >= LOCK_CONFIRM_TICKS)
+            {
+                radar_state = RADAR_STATE_LOCK;
+                loss_timer  = 0U;
+            }
+        }
+        else
+        {
+            radar_state = RADAR_STATE_SEARCH;
+        }
+        break;
+    case RADAR_STATE_LOCK:
+        if (target_detected == false)
+        {
+            loss_timer++;
+            if (loss_timer >= LOCK_LOSS_TICKS)
+            {
+                radar_state = RADAR_STATE_SEARCH;
+            }
+        }
+        else
+        {
+            loss_timer = 0U;
+        }
+        break;
+    default:
+        radar_state = RADAR_STATE_SEARCH;
+        break;
+    }
+}
+
 int main(void)
 {
     return 0;
