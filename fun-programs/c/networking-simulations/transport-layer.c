094c5614be1b1a5bb996537cbdc10a46dbf84eda
Premanshu Bhagat <premtsapj@gmail.com>
2026-01-21 15:57:15 -0500

    added skeleton for a mode based tcp udp transport sim

---
+++ fun-programs/c/networking-simulations/transport-layer.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <windows.h>
+
+#define MAX_PDU_PER_TRANSPORT 1000
+
+typedef struct
+{
+
+}segment_t;
+
+typedef struct
+{
+
+}datagram_t;
+
+
+typedef struct
+{
+
+}udp_t;
+
+typedef struct
+{
+
+}tcp_t;
+
+typedef enum mode{
+    UDP, // 0
+    TCP  // 1
+} mode_t;
+
+int main(void)
+{
+    int numPDU      = MAX_PDU_PER_TRANSPORT - 1000;
+    mode_t protocol = 0;
+
+    return EXIT_SUCCESS;
+}
