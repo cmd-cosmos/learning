cf123184c57858493995c7949c1807b0f79855db
Premanshu Bhagat <premtsapj@gmail.com>
2026-01-21 18:04:39 -0500

    added structs for the TCP segment header and the UDP datagram header

--- fun-programs/c/networking-simulations/transport-layer.c
+++ fun-programs/c/networking-simulations/transport-layer.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <windows.h>
 
 #define MAX_PDU_PER_TRANSPORT 1000
+#define MAX_DATA_SIZE         1460 // bytes
 
+
+/**
+ * @brief TCP segment header format 
+ * 
+ */
 typedef struct 
 {
-    
+    uint16_t sourcePort;
+    uint16_t destinationPort;
+    uint32_t sequenceNumber;
+    uint32_t offsetAndFlags;
+    uint16_t checksum;
+    uint16_t urgentPointer;
+    uint16_t options;
+    uint16_t padding;
 }segment_header_t;
 
+
+/**
+ * @brief UDP datagram header format 
+ * 
+ */
 typedef struct 
 {
-    
+    uint16_t sourcePort;
+    uint16_t destinationPort;
+    uint16_t length;
+    uint16_t checksum;   
 }datagram_header_t;
 
 typedef struct
