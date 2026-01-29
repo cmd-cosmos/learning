dd815dd44ad0bf81cc9529d811ccd228d39a4d7c
Premanshu Bhagat <premtsapj@gmail.com>
2026-01-28 21:34:12 -0500

    POST request and render template

--- tutorials/python/webdev/app.py
+++ tutorials/python/webdev/app.py
@@ -1,13 +1,23 @@
 #pylint: skip-file
 #type: ignore
 
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/intro")
 def foo():
     return "I am Batman"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        return f"Username: {username} | Password: {password}"
+    else:
+        return render_template('login.html')
 
 if __name__ == "__main__":
     app.run()
