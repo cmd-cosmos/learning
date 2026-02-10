ff8f5b0d4a92efee444bb1bd9b205a0dd1ca35ab
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-10 17:36:50 -0500

    added a basic java program for refreshing syntax knowledge

---
+++ tutorials/java/misc_revision/basics/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    @SuppressWarnings("unused")
+    public static void main(String args[])
+    {
+        System.out.println("I am Batman");
+
+        int age = 22;
+        double salary = 10000.00;
+        char grade = 'F';
+        boolean goLaunch = true;
+        String name = "Batman";
+
+        System.out.println(name + " is " + age + " years old\n" + salary + '\n' + grade);
+
+    }
+}
