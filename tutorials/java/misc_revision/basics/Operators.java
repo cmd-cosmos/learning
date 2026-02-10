7770ae31a6d596e3b557db1d211131d11aa2f47c
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-10 17:41:13 -0500

    basic operators in java

---
+++ tutorials/java/misc_revision/basics/Operators.java
@@ -0,0 +1,16 @@
+public class Operators{
+    @SuppressWarnings("unused")
+    public static void main(String args[])
+    {
+        int x = 100, y = 20;
+
+        System.out.println("x = " + x + "\ny = " + y);
+        System.out.println("x + y = " + (x + y));
+        System.out.println("x - y = " + (x - y));
+        System.out.println("x * y = " + (x * y));
+        System.out.println("x / y = " + (x / y));
+        System.out.println("x % y = " + (x % y));
+        System.out.println("x > y = " + (x > y));
+
+    }
+}
