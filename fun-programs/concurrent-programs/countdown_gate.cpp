d77419305a18e72cc5250a617103e79b95711adb
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-09 20:57:56 -0500

    engine chill function

--- fun-programs/concurrent-programs/countdown_gate.cpp
+++ fun-programs/concurrent-programs/countdown_gate.cpp
@@ -8,3 +8,14 @@
 std::mutex mtx;
 std::condition_variable cv;
 bool engine_ready = false;
+
+void engine_chill()
+{
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        engine_ready = true;
+    }
+    std::cout << "[ENGINE] Chilldown Complete\n";
+    cv.notify_one();
+}
