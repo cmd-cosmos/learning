7445b4cd7c02eb43c68ac3914177e37fc3430bbf
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-10 19:08:25 -0500

    added comparision and logical operators

--- tutorials/java/misc_revision/basics/Operators.java
+++ tutorials/java/misc_revision/basics/Operators.java
@@ -3,14 +3,23 @@ public class Operators{
     public static void main(String args[])
     {
         int x = 100, y = 20;
-
+        boolean a = true, b = false;
         System.out.println("x = " + x + "\ny = " + y);
+        System.out.println("a = " + a + "\nb = " + b);
         System.out.println("x + y = " + (x + y));
         System.out.println("x - y = " + (x - y));
         System.out.println("x * y = " + (x * y));
         System.out.println("x / y = " + (x / y));
         System.out.println("x % y = " + (x % y));
-        System.out.println("x > y = " + (x > y));
-
+        System.out.println("x > y  ? " + (x > y));
+        System.out.println("x == y ? " + (x == y));
+        System.out.println("x != y ? " + (x != y));
+        System.out.println("x >= y ? " + (x >= y));
+        System.out.println("x <= y ? " + (x <= y));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("a & b = " + (a && b));
+        System.out.println("!a = " + (!a));
+        System.out.println("!b = " + (!b));
+    
     }
 }
