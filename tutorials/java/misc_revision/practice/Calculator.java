0f46a403f32f663189c491914a817294fa24ba6c
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-10 22:33:53 -0500

    basic calculator console app

--- tutorials/java/misc_revision/practice/Calculator.java
+++ tutorials/java/misc_revision/practice/Calculator.java
@@ -7,6 +7,7 @@ public class Calculator
     public static void main(String args[])
     {
         Scanner scanner = new Scanner(System.in);
+        int op_id = 0;
         System.out.println("======== CALCULATOR ========");
 
         String[] operations_list = {"+", "-", "*", "/", "%", "^"};
@@ -17,12 +18,48 @@ public class Calculator
         try
         {
             System.out.print("Choose Operation ID: ");
-            int op_id = scanner.nextInt();
+            op_id = scanner.nextInt();
             System.out.println("\nSelected OP ID | " + op_id + " | " + operations_list[op_id] + '\n');
         }
         catch (IndexOutOfBoundsException e)
         {
             System.err.println("IndexOutOfBoundsException: " + e.getMessage());
+        }
+
+        System.out.print("Enter num 1: ");
+        double num1 = scanner.nextInt();
+        System.out.print("Enter num 2: ");
+        double num2 = scanner.nextInt();
+
+        double result;
+        switch (operations_list[op_id]) {
+            case "+":
+                result = num1 + num2;
+                System.out.println("\nResult: " + result);
+                break;
+            case "-":
+                result = num1 - num2;
+                System.out.println("\nResult: " + result);
+                break;
+            case "*":
+                result = num1 * num2;
+                System.out.println("\nResult: " + result);
+                break;
+            case "/":
+                result = num1 / num2;
+                System.out.println("\nResult: " + result);
+                break;
+            case "%":
+                result = num1 % num2;
+                System.out.println("\nResult: " + result);
+                break;
+            case "^":
+                result = Math.pow(num1, num2);
+                System.out.println("\nResult: " + result);
+                break;
+            default:
+                System.out.println("\nError");
+                break;
         }
         
         scanner.close();
