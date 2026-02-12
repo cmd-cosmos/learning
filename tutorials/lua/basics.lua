614bdf5e8a0bf28aa5cf8a9c11c5fd01b6b3ae7c
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-11 21:10:04 -0500

    added basics of lua programming lang

---
+++ tutorials/lua/basics.lua
@@ -0,0 +1,20 @@
+-- dynamically typed
+
+name = "batman"
+age = 22
+pi = 3.1415
+is_active = true
+
+-- local vs global
+-- by default vars are global
+x = 100 
+
+-- use local keyword
+local y = 100
+
+-- string concat
+-- .. is used for concatenation
+local greet = "Hello"
+local user = "batman"
+
+print(greet .. " " .. name)
