d59be2796c30f756b8fe7da820fe45d478307ab9
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-14 12:03:48 -0500

    using extern keyword to ensure only declaration and no definition

---
+++ tutorials/c/misc/math_utils.h
@@ -0,0 +1,12 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+int add(int a, int b);
+
+// considered as a definition
+// int global_val;
+
+// using the extern keyword --> declaration only no definition
+extern int global_val;
+
+#endif
