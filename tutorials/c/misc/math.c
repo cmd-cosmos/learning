5ea17e4f144726d3ed84c6a7c280bee9ab9e5e69
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-14 12:08:46 -0500

    modifying global vals that are declared in header files

--- tutorials/c/misc/math.c
+++ tutorials/c/misc/math.c
@@ -5,5 +5,13 @@ int main()
 {
     printf("sum: %d\n", add(2,3));
     printf("global val: %d\n", global_val);
+
+    global_val += 5000;
+    
+    printf("modified global val: %d\n", global_val);
+    
+    int global_val = 6000;
+    printf("modified global val: %d\n", global_val);
+
     return 0;
 }
