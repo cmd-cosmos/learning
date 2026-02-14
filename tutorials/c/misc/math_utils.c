39be74b4923bba2cf1e78e3b07a707422da6d34b
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-14 12:16:04 -0500

    using static to ensure internal linkage

--- tutorials/c/misc/math_utils.c
+++ tutorials/c/misc/math_utils.c
@@ -1,8 +1,11 @@
 #include "math_utils.h"
 
+// static ensures internal linkage so only accessible to current translation units
+static int hidden = 42;
+
 int global_val = 100;
 
 int add(int a, int b)
 {
-    return a + b;
+    return a + b + hidden;
 }
