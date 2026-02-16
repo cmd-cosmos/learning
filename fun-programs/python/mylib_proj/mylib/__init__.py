26618095e1de592f293934b8570f6bb00295ede0
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-16 14:06:56 -0500

    init file to make the folder a package

---
+++ fun-programs/python/mylib_proj/mylib/__init__.py
@@ -0,0 +1,14 @@
+# pylint: skip-file
+
+from .math_utils import add, subtract, multiply, divide
+from .string_utils import to_uppercase, to_lowercase, count_vowels
+
+__all__ = [
+    "add",
+    "subtract",
+    "multiply",
+    "divide",
+    "to_uppercase",
+    "to_lowercase",
+    "count_vowels"
+]
