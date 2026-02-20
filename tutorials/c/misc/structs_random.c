ab12c0e9a2e1133cb8a231007ae1db09b20cf40a
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-19 21:36:02 -0500

    using pointers in structs and different deref methods

--- tutorials/c/misc/structs_random.c
+++ tutorials/c/misc/structs_random.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     uint32_t x;
     uint32_t y;
 } vector_t;
+
+typedef struct
+{
+    char* name;
+    uint32_t age;
+} student_t;
 
 int main(void)
 {
@@ -13,6 +21,20 @@ int main(void)
     printf("%d\n",vec.y);
     printf("sizeof struct vector = %zu bytes\n", sizeof(vector_t));
     printf("sizeof vec = %zu bytes\n", sizeof(vec));
+
+    student_t* s1 = malloc(sizeof(student_t));
+    s1->name = "batman";
+    s1->age = 22;
+
+    printf("student name: %s\n", (*s1).name);
+    printf("student age: %d\n", (*s1).age);
+
+    student_t* s2 = malloc(sizeof(student_t));
+    s2->name = "joker";
+    s2->age = 25;
+
+    printf("student name: %s\n", s2->name);
+    printf("student age: %d\n", s2->age);
 
     return 0;
 }
