332c846449042445a2f1fa19f01c8a56bac3864e
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-19 22:22:04 -0500

    added definition to the enqueue function

--- fun-programs/c/misc/scheduler.c
+++ fun-programs/c/misc/scheduler.c
@@ -13,7 +13,22 @@ void scheduler_init(scheduler_t* scheduler)
 
 void scheduler_enqueue(scheduler_t* scheduler, job_t* job)
 {
-    
+    queue_t* q = &scheduler->queue[job->priority];
+
+    job->before = NULL;
+    job->after = NULL;
+
+    if (q->tail == NULL)
+    {
+        q->head = job;
+        q->tail = job;
+    }
+    else
+    {
+        q->tail->after = job;
+        job->before = q->tail;
+        q->tail = job;
+    }
 }
 
 job_t* scheduler_dequeue(scheduler_t* scheduler)
