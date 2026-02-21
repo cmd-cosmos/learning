ac967ce8ca527bc8e99c220a0cc37e0963310e31
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-21 13:32:07 -0500

    using ptr arithmetic to traverse an arr

--- tutorials/c/misc/memory/addr_details.c
+++ tutorials/c/misc/memory/addr_details.c
@@ -8,6 +8,13 @@ void ptr_arithmetic()
     printf("base addr arr = %p\n", (void*)arr);
     printf("addr &addr[0] = %p\n", (void*)&arr[0]);
     printf("using &arr    = %p\n", (void*)&arr);
+
+    // arr expands to &addr[0]
+    int* ptr = arr;
+
+    for (int i = 0; i < 5; i++) {
+        printf("*(p + %d) = %d | address = %p\n", i, *(ptr+i), (void*)(ptr+i));
+    }
 }
 
 int main(void)
