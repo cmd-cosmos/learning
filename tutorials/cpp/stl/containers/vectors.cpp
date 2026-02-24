baa6a9ea9e3fa66b3ae36dca99bb5c4f984dab38
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-24 16:16:24 -0500

    added push_back oper and indexing

--- tutorials/cpp/stl/containers/vectors.cpp
+++ tutorials/cpp/stl/containers/vectors.cpp
@@ -5,6 +5,7 @@ int main(void)
 {
     std::vector<int> nums = {1,2,3,4};
 
+    // iter using range based for loop
     for(int x : nums)
     {
         std::cout << x << " ";
@@ -15,6 +16,16 @@ int main(void)
     {
         std::cout << *iter << " ";
     } std::cout << std::endl;
+
+    std::vector<std::string> test;
+
+    test.push_back("A");
+    test.push_back("B");
+    test.push_back("C");
+    test.push_back("D");
+
+    std::cout << "test[1] = " << test[1] << std::endl;
+    std::cout << "test[3] = " << test[3] << std::endl;
 
     return EXIT_SUCCESS;
 }
