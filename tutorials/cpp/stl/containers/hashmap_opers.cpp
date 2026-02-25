fb267ad97ed60a40c435716cd8f6c7d803c28056
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-25 18:30:47 -0500

    using the at method and size method, printing bools as string instead of binary 0 1

--- tutorials/cpp/stl/containers/hashmap_opers.cpp
+++ tutorials/cpp/stl/containers/hashmap_opers.cpp
@@ -19,13 +19,18 @@ int main(void)
     hashmap[4] = "proton";
     
     printMap(hashmap);
-    
+    std::cout << std::boolalpha;
+
     std::cout << "hashmap contains key=1: " << hashmap.contains(1) << std::endl;
 
     hashmap.insert(std::map<int, std::string>::value_type(5, "electron"));
     hashmap.insert(std::map<int, std::string>::value_type(5, "electron"));
     
     printMap(hashmap);
+
+    std::cout << "hashmap.at(3): " << hashmap.at(3) << std::endl;
+
+    std::cout << "hashmap size: " << hashmap.size() << " key val pairs\n";
 
     return 0;
 }
