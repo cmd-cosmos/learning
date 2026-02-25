130e3934ab634cf4070bdc05e60157f07ca4afea
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-25 17:36:33 -0500

    using map insert method and contains method

---
+++ tutorials/cpp/stl/containers/hashmap_opers.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <map>
+
+void printMap(const auto& hashmap)
+{
+    for(auto& entry : hashmap)
+    {
+        std::cout << entry.first << " : " << entry.second << std::endl;
+    }
+}
+
+int main(void)
+{
+    std::map<int, std::string> hashmap;
+
+    hashmap[1] = "falcon";
+    hashmap[2] = "starship";
+    hashmap[3] = "vulcan";
+    hashmap[4] = "proton";
+    
+    printMap(hashmap);
+    
+    std::cout << "hashmap contains key=1: " << hashmap.contains(1) << std::endl;
+
+    hashmap.insert(std::map<int, std::string>::value_type(5, "electron"));
+    hashmap.insert(std::map<int, std::string>::value_type(5, "electron"));
+    
+    printMap(hashmap);
+
+    return 0;
+}
