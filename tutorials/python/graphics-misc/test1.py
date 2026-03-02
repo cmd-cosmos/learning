ac68c1431113d77d7e133cbef8678dc3a1fffe48
Premanshu Bhagat <premtsapj@gmail.com>
2026-03-02 18:09:28 -0500

    rectangles in pygame

--- tutorials/python/graphics-misc/test1.py
+++ tutorials/python/graphics-misc/test1.py
@@ -6,13 +6,15 @@ import random
 pygame.init()
 pygame.display.set_caption("Test Window")
 screen = pygame.display.set_mode((400, 500))
-screen.fill((255,255,0))
+screen.fill((255,255,255))
 pygame.display.update()
+rect = pygame.Rect(10, 10, 20, 20)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        screen.fill((random.randint(0,255), random.randint(0,255), random.randint(0,255)))
+        pygame.draw.rect(screen, color="red", rect=rect)
+        pygame.display.flip()
         pygame.display.update()
