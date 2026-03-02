a9831c149cdd3b7f31e30a9f1163dc4c6490c2d1
Premanshu Bhagat <premtsapj@gmail.com>
2026-03-02 15:31:28 -0500

    added color and caption to window and removed custom class for now

--- tutorials/python/graphics-misc/test1.py
+++ tutorials/python/graphics-misc/test1.py
@@ -3,18 +3,13 @@ import pygame
 import sys
 
 pygame.init()
-pygame.display.set_mode((500, 200))
-
-class Ball:
-    def __init__(self, radius) -> None:
-        self.radius = radius
-    
-    def draw(self):
-        pass
+pygame.display.set_caption("Test Window")
+screen = pygame.display.set_mode((400, 500))
+screen.fill((255,255,0))
+pygame.display.update()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        pygame.display.update()
