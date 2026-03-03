c10665af89bc0f26dc296e6063735da4dee3e90f
Premanshu Bhagat <premtsapj@gmail.com>
2026-03-03 18:34:03 -0500

    sum function for variable number of args

---
+++ tutorials/c/c-adv-course/module2/varargs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum(int count, ...)
+{
+    va_list args;
+    va_start(args, count);
+
+    int total = 0;
+    for (int i = 0; i < count; i++)
+    {
+        total += va_arg(args, int);
+    }
+
+    va_end(args);
+    return total;
+}
+
+int main()
+{
+    return 0;
+}
