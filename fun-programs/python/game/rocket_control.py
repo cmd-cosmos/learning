81691aec87e05ed4245248e36f5a82bacd0fdfbc
Premanshu Bhagat <premtsapj@gmail.com>
2026-03-02 19:50:57 -0500

    display the rocket sprite on screen

--- fun-programs/python/game/rocket_control.py
+++ fun-programs/python/game/rocket_control.py
@@ -1,10 +1,35 @@
 #pylint: skip-file
 import pygame
+import sys
+from pygame.locals import *
 
 COLOR = (255,255,255)
 WIDTH = 500
 HEIGHT = 800
 
+SURFACE = pygame.display.set_mode((WIDTH, HEIGHT))
+
+FPS = pygame.time.Clock()
+FPS.tick(60)
+
+
+
 class RocketSprite(pygame.sprite.Sprite):
     def __init__(self) -> None:
-        pass
+        super().__init__()
+        self.image = pygame.image.load("media/rocket-2.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (100, 100)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+rocket = RocketSprite()
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        rocket.draw(SURFACE)
+        pygame.display.update()
+
