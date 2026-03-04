7cb193dc4e10cf229609deab0775496774fafe24
Premanshu Bhagat <premtsapj@gmail.com>
2026-03-03 19:30:42 -0500

    tokenizing using strtok function

--- tutorials/c/c-adv-course/module4/mem.c
+++ tutorials/c/c-adv-course/module4/mem.c
@@ -18,5 +18,14 @@ int main()
         printf("%d ", arr[i]);
     } printf("\n");
 
+    char str[] = "one,two,three";
+    char* token = strtok(str, ",");
+
+    while (token)
+    {
+        printf("%s\n", token);
+        token = strtok(NULL, ",");
+    }
+    
     return 0;
 }
