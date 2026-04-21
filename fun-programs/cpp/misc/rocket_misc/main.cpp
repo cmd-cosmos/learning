21c2e3fdb62a09026f95eea662808e0631ad0012
Premanshu Bhagat <premtsapj@gmail.com>
2026-04-21 17:26:23 -0400

    added decimal precision using setprecision

--- fun-programs/cpp/misc/rocket_misc/main.cpp
+++ fun-programs/cpp/misc/rocket_misc/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <iomanip>
 #include "Rocket.h"
 
 int main()
 {
     Rocket rocket(1000, 500, 20000);
-
+    std::cout << std::fixed;
+    std::cout << std::setprecision(2);
     for (int i = 0; i < 100; i++)
     {
         rocket.update(0.1);
