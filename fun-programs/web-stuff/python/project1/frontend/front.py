a77cb241c869482a3f4eff49ad3a36d3d475a095
Premanshu Bhagat <premtsapj@gmail.com>
2026-05-17 10:02:47 +0530

    basic form

--- fun-programs/web-stuff/python/project1/frontend/front.py
+++ fun-programs/web-stuff/python/project1/frontend/front.py
@@ -5,8 +5,19 @@ import streamlit as st
 
 st.title("User Registration")
 
-st.text_input(label="ID", placeholder="enter user id")
-st.text_input(label="Name", placeholder="enter user name")
+if "uid" not in st.session_state:
+    st.session_state.uid = ""
 
-if st.button("Register"):
+if "uname" not in st.session_state:
+    st.session_state.uname = ""
+
+with st.form("registration_form"):
+    st.text_input(label="ID", placeholder="enter user id", key="uid")
+    st.text_input(label="Name", placeholder="enter user name", key="uname")
+
+    submitted = st.form_submit_button("Register")
+
+if submitted:
     st.success("user registered")
+    st.session_state.uid = ""
+    st.session_state.uname = ""
