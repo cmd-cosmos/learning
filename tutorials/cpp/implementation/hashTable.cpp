b2d74d4103239663dc7c58f2823217845970b711
Premanshu Bhagat <premtsapj@gmail.com>
2026-05-22 20:58:58 +0530

    created a basic hashtable class

---
+++ tutorials/cpp/implementation/hashTable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+#include <list>
+using namespace std;
+
+class HashTable {
+    private:
+        static const int hashGroups = 10;
+        list<pair<int, string>> table[hashGroups];
+    
+};
+
+int main()
+{
+
+
+    return 0;
+}
