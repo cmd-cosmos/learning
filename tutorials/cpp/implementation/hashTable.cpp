66e61ff9af74c05502d2be3599d92d4efdd2f5e3
Premanshu Bhagat <premtsapj@gmail.com>
2026-05-24 19:53:56 +0530

    added hashing function and insertItem method

--- tutorials/cpp/implementation/hashTable.cpp
+++ tutorials/cpp/implementation/hashTable.cpp
@@ -29,6 +29,30 @@ bool HashTable::isEmpty() const {
     } return false;
 }
 
+int HashTable::hashFunction(int key) {
+    return key % hashGroups;
+}
+
+void HashTable::insertItem(int key, string value) {
+    int hashValue = hashFunction(key);
+    auto& cell = table[hashValue];
+    auto bItr = begin(cell);
+    bool keyExists = false;
+    for (; bItr != end(cell); bItr++) {
+        if (bItr->first  == key) {
+            keyExists = true;
+            bItr->second = value;
+            cout << "[WARNING] Key exists. Value replaced." << endl;
+            break;
+        }
+    }
+
+    if (!keyExists) {
+        cell.emplace_back(key, value);
+    }
+    return;
+}
+
 int main()
 {
 
