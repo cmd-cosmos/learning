ed9d3269240278a5e8b49074809193654311244e
Premanshu Bhagat <premtsapj@gmail.com>
2026-06-05 21:54:15 +0530

    added echo response for the bot

--- fun-programs/apps/echo-bot/main.py
+++ fun-programs/apps/echo-bot/main.py
@@ -8,7 +8,7 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 for message in st.session_state.messages:
-    with st.chat_message(msg["role"]):
+    with st.chat_message(message["role"]):
         st.markdown(message["content"])
     
 user_inp = st.chat_input("Enter Query...")
@@ -21,4 +21,12 @@ if user_inp:
     with st.chat_message("user"):
         st.markdown(user_inp)
 
-        bot_response = f"[ECHO] {user_inp}"
+    bot_response = f"[ECHO] {user_inp}"
+    st.session_state.messages.append(
+            {
+                "role" : "assistant",
+                "content" : bot_response
+            }
+        )
+    with st.chat_message("assistant"):
+        st.markdown(bot_response)
