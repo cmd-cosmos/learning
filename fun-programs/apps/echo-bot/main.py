729ccdfebf7fac50f8aa6c19636c5fdaaf99f1a1
Premanshu Bhagat <premtsapj@gmail.com>
2026-06-05 21:44:25 +0530

    added a basic user query for the chat bot

---
+++ fun-programs/apps/echo-bot/main.py
@@ -0,0 +1,24 @@
+#type: ignore
+#pylint: skip-file
+import streamlit as st
+
+st.title("Echo Bot")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.markdown(message["content"])
+    
+user_inp = st.chat_input("Enter Query...")
+
+if user_inp:
+    st.session_state.messages.append({
+        "role" : "user",
+        "content" : user_inp
+    })
+    with st.chat_message("user"):
+        st.markdown(user_inp)
+
+        bot_response = f"[ECHO] {user_inp}"
