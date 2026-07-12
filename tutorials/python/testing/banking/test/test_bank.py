aa58edd5e354a65f1a59dec9291f2a4a307292f3
Premanshu Bhagat <premtsapj@gmail.com>
2026-07-12 18:46:44 +0530

    added testing suite for the banking program

---
+++ tutorials/python/testing/banking/test/test_bank.py
@@ -0,0 +1,39 @@
+#pylint: skip-file
+
+import pytest
+from src.bank import BankAccount
+
+def test_account_creation():
+    account = BankAccount("Batman")
+    
+    assert account.holder == "Batman"
+    assert account.balance == 0
+    
+def test_deposit_changes_balance():
+    account = BankAccount("Batman")
+    account.deposit(1000)
+    
+    assert account.balance == 1000
+
+def test_multiple_deposits():
+    account = BankAccount("Batman")
+    account.deposit(100)
+    account.deposit(200)
+    
+    assert account.balance == 300
+    
+def test_withdraw_changes_balance():
+    account = BankAccount("Batman")
+    account.deposit(1000)
+    account.withdraw(500)
+    
+    assert account.balance == 500
+
+def test_withdraw_more_than_balance():
+    account = BankAccount("Batman")
+    account.deposit(1000)
+    
+    with pytest.raises(ValueError):
+        account.withdraw(2000)    
+    
+    
